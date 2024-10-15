7403585f037a3748b7aec54063149cb5067e1001
rye1014 <rye10140210@gmail.com>
2024-10-15 14:02:36 +0100

    Add Practical05 Codes

--- practical05/prac05-1.c
+++ practical05/prac05-1.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
-void fibonacci(int fn_2, int fn_1, int *p_fn_1, int *p_fn)
+// compute Fibonacci series
+// input: F_n-2, F_n-1
+// output: F_n-1, F_n
+void fibonacci(int fn_2, int fn_1, int *p_fn_1, int *p_fn)  
 {
     *p_fn = fn_2 + fn_1; 
     *p_fn_1 = fn_1;  
 }
 
-
 int main(void)
 {
     int n; 
-    int a = 0, b = 1; 
+    int a = 0, b = 1;               // initialize a = F_0 = 0, b = F_1 = 1
 
-    scanf("%d", &n); 
+    scanf("%d", &n);                // enter the value n
 
-    printf("%d %d ", a, b); 
+    printf("%d %d ", a, b);         
     for (int i=2; i<=n; i++)
     {
-        fibonacci(a, b, &a, &b); 
+        fibonacci(a, b, &a, &b);    // update the values of a and b,
+                                    // which is equivalent to shifting one position to the right in the Fibonacci series
         printf("%d ", b); 
-
     }
+    printf("\n"); 
 
     return 0; 
 }
---
+++ practical05/prac05-2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h> 
+#include <float.h>
+
+double maclaurin(double x, int n)       // compute the single element in Maclaurin series: (x^(2n+1)) / (2n+1)
+{
+    return (pow(x, 2.0 * n + 1.0)) / (2.0 * n + 1.0); 
+}
+
+double arctanh1(double x, double delta) // compute the arctanh by Maclaurin series
+{
+    int n = 0;
+    double mac = DBL_MAX; 
+    double sum = 0.0;  
+
+    while (fabs(mac) >= delta)                // stop when the absolute value of element is smaller than delta
+    {
+        mac = maclaurin(x, n);          // update the element 
+        sum += mac;                     // sum the Maclaurin series up
+        n++; 
+    }
+
+    return sum; 
+}
+
+
+double arctanh2(double x)               // compute the arctanh by natural logarithms
+{
+    return (log(1.0 + x) - log(1.0 - x)) / 2.0;     
+}
+
+
+int main(void)
+{
+    double delta;               // declare a user-defined delta
+    scanf("%lf", &delta); 
+
+    double upper = 0.9, lower = -0.9, interval = 0.01;      // x in [-0.9, 0.9], sampled every 0.01
+    int size = (upper - lower) / interval + 1;              // the total number of samples: 181
+
+    double result1[size], result2[size];                    // declare two array to store the results for both methods
+
+    for (int i=0; i<size; i++)                              // store them into arrays
+    {
+        result1[i] = arctanh1(lower + interval * i, delta); 
+        result2[i] = arctanh2(lower + interval * i); 
+    }
+
+    double diff; 
+    for (int i=0; i<size; i++)                              // compare their differences
+    {
+        diff = fabs(result1[i] - result2[i]); 
+        printf("%.2f  %.10f %.10f %.10f\n", lower + interval * i, result1[i], result2[i], diff); 
+    }
+
+    return 0; 
+}
